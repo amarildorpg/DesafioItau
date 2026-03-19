91c977500c63041d0ce9c679fb092e7d27277c57
amarildorpg <amarildorpg@gmail.com>
2026-03-18 21:21:25 -0400

    feat: criação dos DTOs

---
+++ src/main/java/dev/datanorte/DesafioItau/dto/TransacaoDTO.java
@@ -0,0 +1,9 @@
+package dev.datanorte.DesafioItau.dto;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+public class TransacaoDTO {
+    private BigDecimal valor;
+    private OffsetDateTime dataHora;
+}
