fb2b0a88ff16f77410da10f049475364a963763f
amarildorpg <amarildorpg@gmail.com>
2026-03-18 22:57:24 -0400

    feat: criar repository de transação

--- src/main/java/dev/datanorte/DesafioItau/controllers/TransacoesController.java
+++ src/main/java/dev/datanorte/DesafioItau/controllers/TransacoesController.java
@@ -1,31 +1,47 @@
 package dev.datanorte.DesafioItau.controllers;
 
 import dev.datanorte.DesafioItau.dto.TransacaoDTO;
+import dev.datanorte.DesafioItau.repository.TransacaoRepository;
 import dev.datanorte.DesafioItau.service.TransacaoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transacao")
 public class TransacoesController {
-    private final TransacaoService transacaoService;
-    public TransacoesController(TransacaoService transacaoService) {
+    private TransacaoRepository transacaoRepository;
+
+    public TransacoesController(TransacaoRepository transacaoRepository, TransacaoService transacaoService) {
+        this.transacaoRepository = transacaoRepository;
         this.transacaoService = transacaoService;
     }
+
+    private TransacaoService transacaoService;
+
 
     @PostMapping
     public ResponseEntity<Void> adicionar(@RequestBody TransacaoDTO transacaoDTO) {
         try {
             transacaoService.validarTransacao(transacaoDTO);
+            transacaoRepository.salvarDados(transacaoDTO);
             return ResponseEntity.status(HttpStatus.CREATED).build();
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(422).build();
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
 
+    }
+    @DeleteMapping
+    public ResponseEntity<Void> excluir(@RequestBody TransacaoDTO transacaoDTO) {
+        try {
+            transacaoService.validarTransacao(transacaoDTO);
+            return  ResponseEntity.status(HttpStatus.OK).build();
+
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.status(422).build();
+        }
     }
 
 }
---
+++ src/main/java/dev/datanorte/DesafioItau/repository/TransacaoRepository.java
@@ -0,0 +1,25 @@
+package dev.datanorte.DesafioItau.repository;
+
+import dev.datanorte.DesafioItau.dto.TransacaoDTO;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+@Repository
+public class TransacaoRepository {
+    List<TransacaoDTO> listaTransacoes= new ArrayList<>();
+    //Salvar os dados em uma lista
+    public void salvarDados(TransacaoDTO transacaoDTO) {
+        listaTransacoes.add(transacaoDTO);
+    }
+
+    //Apagar essa lista após 60 segundos
+    public void limparDados(){
+
+    }
+
+    //Apagar todas as transações da lista
+    public void deletarDados(){
+        listaTransacoes.clear();
+    }
+}
