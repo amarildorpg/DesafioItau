ca4b84d953f00b9ebe2bd8b754944ccb6139e250
amarildorpg <amarildorpg@gmail.com>
2026-03-19 23:55:45 -0400

    feat: Implementando Logs

--- src/main/java/dev/datanorte/DesafioItau/controllers/EstatisticasController.java
+++ src/main/java/dev/datanorte/DesafioItau/controllers/EstatisticasController.java
@@ -2,12 +2,14 @@ package dev.datanorte.DesafioItau.controllers;
 
 import dev.datanorte.DesafioItau.properties.EstatisticaProperties;
 import dev.datanorte.DesafioItau.repository.TransacaoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.OffsetDateTime;
 
+@Slf4j
 @RestController
 @RequestMapping("/estatistica")
 public class EstatisticasController {
@@ -20,6 +22,7 @@ public class EstatisticasController {
 
     @GetMapping
     public ResponseEntity<?> estatisticas() {
+        log.info("Calculando estatísticas");
         final var horaInicial = OffsetDateTime
                 .now()
                 .minusSeconds(properties.segundos());
--- src/main/java/dev/datanorte/DesafioItau/controllers/TransacoesController.java
+++ src/main/java/dev/datanorte/DesafioItau/controllers/TransacoesController.java
@@ -3,10 +3,11 @@ package dev.datanorte.DesafioItau.controllers;
 import dev.datanorte.DesafioItau.dto.TransacaoDTO;
 import dev.datanorte.DesafioItau.repository.TransacaoRepository;
 import dev.datanorte.DesafioItau.service.TransacaoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@Slf4j
 @RestController
 @RequestMapping("/transacao")
 public class TransacoesController {
@@ -21,11 +22,13 @@ public class TransacoesController {
 
     @PostMapping
     public ResponseEntity<Void> adicionar(@RequestBody TransacaoDTO transacaoDTO) {
+        log.info("Adicionando transação");
         try {
             transacaoService.validarTransacao(transacaoDTO);
             transacaoRepository.salvarDados(transacaoDTO);
             return ResponseEntity.status(HttpStatus.CREATED).build();
         } catch (IllegalArgumentException e) {
+            log.error("Erro e uma ou mais validações, tente novamente");
             return ResponseEntity.status(422).build();
         } catch (Exception e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
